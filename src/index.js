3947b6ea20b3b8507762fded919f9f0049a4616e
Kristyvdm2020 <kristyvdm2020@gmail.com>
2022-12-12 11:17:28 -0500

    Added Routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import React, { useState, useEffect } from 'react';
+import {HashRouter, Routes, Route, Link } from 'react-router-dom'
 
 const Products = (props) => {
   const {products} = props;
@@ -38,14 +39,15 @@ const Companies = (props) => {
 const App = ()=> {
   const [products, setProducts] = useState([]);
   const [companies, setCompanies] = useState([]);
-  const [view, setView] = useState('');
+  //const [view, setView] = useState('');
 
-  useEffect (()=> {
-    window.addEventListener('hashchange', ()=> {
-      setView(window.location.hash.slice(1));
-    })
-    setView(window.location.hash.slice(1));
-  })
+  // useEffect (()=> {
+  //   window.addEventListener('hashchange', ()=> {
+  //     setView(window.location.hash.slice(1));
+  //   })
+  //   setView(window.location.hash.slice(1));
+  // })
+
   useEffect (()=> {
     fetch('https://www.acme-api.com/api/products')
     .then( response => {
@@ -64,14 +66,20 @@ const App = ()=> {
     <div>
       <h1>Acme Products Companies</h1>
       <nav>
-        <a href='#products'>Products ({products.length})</a>
-        <a href='#companies'>Companies ({companies.length})</a>
+        <Link to='/products'>Products ({products.length}) </Link>
+        {/* <a href='#/products'>Products ({products.length})</a> */}
+        <Link to='/companies'>Companies ({companies.length})</Link>
+        {/* <a href='#/companies'>Companies ({companies.length})</a> */}
       </nav>
-      {view === 'products' ? <Products products={products} />: null}
-      {view === 'companies' ? <Companies companies={companies}/>: null}
+      <Routes>
+        <Route path='/products' element = { <Products products={products} />} />
+        <Route path='/companies' element = { <Companies companies={companies}/> } />
+      </Routes>
+      {/* {view === 'products' ? <Products products={products} />: null}
+      {view === 'companies' ? <Companies companies={companies}/>: null} */}
     </div>
   );
 };
 //const root = ReactDOM.createRoot(document.querySelector('#root'));
 //root.render(<App />);
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<HashRouter><App /></HashRouter>, document.querySelector('#root'));
