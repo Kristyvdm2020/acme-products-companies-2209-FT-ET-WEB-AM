7826dedd667894614a85b09be12e6c3526179e80
ericpkatz <ericpkatz@gmail.com>
2022-12-12 10:09:05 -0500

    commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+
+const App = ()=> {
+ 
+  return (
+    <div>
+      <h1>Acme Products Companies</h1>
+    </div>
+  );
+};
+//const root = ReactDOM.createRoot(document.querySelector('#root'));
+//root.render(<App />);
+ReactDOM.render(<App />, document.querySelector('#root'));
